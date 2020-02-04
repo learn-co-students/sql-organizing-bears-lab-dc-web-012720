7d817a2b58e1b4ce667d7d2a74750c958d5f619b
matteoaricci <matteo.a.ricci@gmail.com>
2020-02-04 13:56:06 -0500

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,1 @@
+create table bears (id INTEGER PRIMARY KEY, name text, age integer, gender char(1), color text, temperament text, alive boolean);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Black", "Docile", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "M", "Blue", "Docile", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, "F", "Brown", "Volatile", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 3, "M", "Brown", "Volatile", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 6, "F", "White", "Docile", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 10, "M", "Green", "Mean One", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "F", "Red", "Docile", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 6, "M", "Brown", "Volatile", 0);
